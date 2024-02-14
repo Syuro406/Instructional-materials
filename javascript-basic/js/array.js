bf4a67c08e11d3113752c0be8047f6cb33b778bb
Syuro406 <145756479+Syuro406@users.noreply.github.com>
2024-02-14 20:12:29 +0900

    Instructional materials

---
+++ javascript-basic/js/array.js
@@ -0,0 +1,17 @@
+// 配列の宣言と代入を行
+const userNames = ['侍太郎', '侍一郎', '侍二郎', '侍四郎'];
+
+// 配置の値を出力する
+console.log(userNames);
+
+// 2番目の要素を更新する
+userNames[1] = '侍花子';
+
+// 6番目に要素を追加する
+userNames[5] = '侍五郎';
+
+// 配置の値を出力する
+console.log(userNames);
+
+  // 3番目の要素を出力する
+  console.log(userNames[2]);
